c30baa2c620f9184ca8881ca336476fbd37e5b49
Job Muturi <muirimuturi17@gmail.com>
2023-03-17 14:36:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventArray) {
+    const cardArray = []      
+        for (let i = 0; i < namesArray.length; i++) {
+
+            cardArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventArray} gift!`)
+            console.log(cardArray)
+                    }
+            return cardArray
+
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+function countDown(input) {
+    let countDown = input
+    while(countDown >= 0) {
+    console.log(countDown--);
+}}
+countDown(4)
